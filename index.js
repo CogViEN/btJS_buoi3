9c8f0892d4cbc26dac7d3d42be4acda2ae30280c
unknown <huynhvangiang0504@example.com>
2023-02-13 20:47:00 +0700

    night

--- index.js
+++ index.js
@@ -1,76 +1,30 @@
-var arr = [];
-function Nhap(){
-    var vl = document.getElementById("numBer").value;  
-    if(vl == null || vl == 0){
-        alert("Nhập số đi mày, bị dở hơi à");
+function tinh(){
+    var n = document.getElementById("numBer").value;
+    if(n == null || n == 0){
+        alert("Nhập số đi mày!");
     }
     else{
-        vl = parseInt(vl);
-        arr.push(vl);
-        document.getElementById("numBer").value = "";
-    }
-}
-
-function xuLi(){
-        var res = "Đây là số chẳn: ";
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i] % 2 == 0){
-                res += arr[i] + " ";
+        n = Number(n);
+        if(n > 13){
+            alert("Nhập số gì lớn dữ ông già");
+        }
+        else{
+            var sum = 0;
+            var arr = [];
+            var used = [];
+            for(let i = 0; i < n; i++){
+                var vl = Math.floor(Math.random() * 20);;
+                while(used[vl]){
+                    vl = Math.floor(Math.random() * 20);
+                }
+                used[vl] = 1;
+                sum += vl;
+                arr.push(vl);
             }
+            document.getElementById("mang").innerHTML = arr;
+            document.getElementById("maxNum").innerHTML = Math.max(...arr);
+            document.getElementById("minNum").innerHTML = Math.min(...arr);
+            document.getElementById("sum").innerHTML = sum;
         }
-        res += '<br>';
-        res += "Đây là số lẻ: ";
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i] % 2 != 0){
-                res += arr[i] + " ";
-            }
-        }
-        res += '<br>';
-        var sum = 0;
-        res += "Tổng phần tử của mảng: ";
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        res += sum;
-        res += '<br>';
-        res += "Số lớn nhất trong mảng: " + Math.max(...arr);
-
-        res += '<br>';
-        res += "Đây là số nguyên tố: ";
-        for(let i = 0; i < arr.length; i++){
-            if(isPrime(arr[i])) res += arr[i] + " ";
-        }
-        res += '<br>';
-        res += "Đây là mã màu tương ứng với số thích hợp " +
-            "để tạo được màu trong phần tử"
-        document.getElementById("kq").innerHTML = res;
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i] >= 0 && arr[i] < 1000000){
-                var div = document.createElement("div");
-                div.style.width = "80px";
-                div.style.height = "20px";
-                div.style.background = taoChuoi(arr[i]);
-                div.style.color = "white";
-                div.innerHTML = taoChuoi(arr[i]);
-                document.getElementById("kq2").appendChild(div);
-            }
-        }
-
-}
-
-function isPrime( n){
-    for(let i = 2; i <= Math.sqrt(n); i++){
-        if(n % i == 0) return 0;
     }
-    return 1;
-}
-
-function taoChuoi( n){
-    var res = "#";
-    let len = n.toString().length;
-    for(; len <= 5; len++){
-        res += '0';
-    }
-    res += n;
-    return res;
 }
